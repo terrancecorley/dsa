455198b0781da799d7b8ed720c84df0752a2d182
Terrance Corley <terrancecorley@outlook.com>
2023-04-22 20:06:42 -0700

    added bubble sort to sorting algos

--- sorting-algorithms/bubble-sort.js
+++ sorting-algorithms/bubble-sort.js
@@ -8,24 +8,11 @@ function swap(arr, index1, index2) {
 }
 
 // My original function
-// function bubbleSort(arr) {
-//     for (let i = 0; i < arr.length - 1; i++) {
-//         for (let j = i + 1; j < arr.length; j++) {
-//             if (arr[i] > arr[j]) {
-//                 swap(arr, i, j);
-//             }
-//         }
-//     }
-
-//     return arr;
-// }
-
-// Function after watching video
-function bubbleSort(arr) {
-    for (let i = arr.length; i > 0; i--) {
-        for (let j = 0; j < i - 1; j++) {
-            if (arr[j] > arr[j + 1]) {
-                swap(arr, j, j + 1);
+function bubbleSortOriginal(arr) {
+    for (let i = 0; i < arr.length - 1; i++) {
+        for (let j = i + 1; j < arr.length; j++) {
+            if (arr[i] > arr[j]) {
+                swap(arr, i, j);
             }
         }
     }
@@ -33,4 +20,34 @@ function bubbleSort(arr) {
     return arr;
 }
 
-console.log(bubbleSort([2, 343, 2, 4, 1, 9, 3]));
+// Unoptimized function after watching video
+// Outer loop moves from end to start
+// inner loop begins at the start, checks the curr index, "bubbles" that value up
+// the key is that we decrement in the outer loop each time we go through an inner loop cycle
+    // basically, we skip needing to do unecessary checks because the values beyond the outer loop pointer are already sorted from prior loops
+// keep track of swaps to end looping early if the arr is already sorted, can assume if we do no swaps that things are sorted
+
+// Big O Time Complexity
+// O(nsquared) average case
+// O(n) is mostly sorted arr already
+function bubbleSort(arr) {
+    for (let i = arr.length; i > 0; i--) {
+        let numOfSwaps = 0;
+        for (let j = 0; j < i - 1; j++) {
+            if (arr[j] > arr[j + 1]) {
+                swap(arr, j, j + 1);
+                numOfSwaps++;
+            }
+        }
+        if (!numOfSwaps) break;
+    }
+
+    return arr;
+}
+
+// Optimized bubble sort function
+
+
+console.log(bubbleSort([4, 1, 2, 3]));
+
+
