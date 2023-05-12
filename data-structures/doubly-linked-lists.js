9a7f2bbcc6022bfe609e75a5d658c3b17154e99d
Terrance Corley <terrancecorley@outlook.com>
2023-05-11 17:16:28 -0700

    added doubly linked list methods

--- data-structures/doubly-linked-lists.js
+++ data-structures/doubly-linked-lists.js
@@ -44,6 +44,109 @@ class DoublyLinkedList {
         this.length--;
         return poppedNode;
     }
+
+    shift() {
+        if (!this.length) return undefined;
+
+        const nodeToRemove = this.head;
+        if (this.length === 1) {
+            this.head = null;
+            this.tail = null;
+        } else {
+            this.head = this.head.next;
+            this.head.prev = null;
+            nodeToRemove.next = null;
+        }
+
+        this.length--;
+        return nodeToRemove;
+    }
+
+    unshift(value) {
+        const newNode = new Node(value);
+        if (!this.length) {
+            this.head = newNode;
+            this.tail = newNode;
+        } else {
+            this.head.prev = newNode;
+            newNode.next = this.head;
+            this.head = newNode;
+        }
+
+        this.length++;
+        return this;
+    }
+
+    get(index) {
+        if (index < 0 || index >= this.length) return undefined;
+        if (index === 0) return this.head;
+        if (index === this.length - 1) return this.tail;
+
+        let counter;
+        let current;
+        if (index <= this.length / 2) {
+            counter = 0;
+            current = this.head;
+            while (counter < index) {
+                current = current.next;
+                counter++
+            }
+        } else {
+            counter = this.length - 1;
+            current = this.tail;
+            while (counter > index) {
+                current = current.prev;
+                counter--;
+            }
+        }
+
+        return current;
+    }
+
+    set(index, value) {
+        const nodeToSet = this.get(index);
+        if (!nodeToSet) return undefined;
+        nodeToSet.value = value;
+        return true;
+    }
+
+    insert(index, value) {
+        if (index < 0 || index >= this.length) return undefined;
+
+        if (index === 0) return !!this.unshift(value);
+        if (index === this.length - 1) return !!this.push(value);
+
+        const newNode = new Node(value);
+        const beforeNode = get(index - 1);
+        const afterNode = beforeNode.next;
+
+        beforeNode.next = newNode;
+        newNode.prev = beforeNode;
+        newNode.next = afterNode;
+        afterNode.prev = newNode;
+        this.length++;
+        return true;
+    }
+
+    remove(index) {
+        if (index < 0 || index >= this.length) return undefined;
+
+        if (index === 0) return this.shift();
+        
+        if (index === this.length - 1) return this.pop();
+
+        const beforeNode = get(index - 1);
+        const nodeToRemove = beforeNode.next;
+        const afterNode = nodeToRemove.next;
+
+        beforeNode.next = afterNode;
+        afterNode.prev = beforeNode;
+        removedNode.prev = null;
+        removedNode.next = null;
+
+        this.length--;
+        return nodeToRemove;  
+    }
 }
 
 const newList = new DoublyLinkedList();
