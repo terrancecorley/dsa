fcf8f4201ac0fe1314635eb7ec9499e26db0c6bb
Terrance Corley <terrancecorley@outlook.com>
2023-06-05 20:06:13 -0700

    added comments for graphs

---
+++ data-structures/graphs.js
@@ -0,0 +1,7 @@
+// * What is a graph?
+// A graph consists of nodes and connections (edges) between them
+
+// * What are some examples that may use graphs?
+// social networks
+// recommendations 
+// map routing 
