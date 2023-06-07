064577e205fffcf9c71caeac9bd4bf5f55c2852b
Terrance Corley <terrancecorley@outlook.com>
2023-06-06 20:15:39 -0700

    added more comments for graphs

--- data-structures/graphs.js
+++ data-structures/graphs.js
@@ -5,3 +5,30 @@
 // social networks
 // recommendations 
 // map routing 
+
+// * Essential graph terms
+// Vertex - a node
+// Edge - connection between nodes
+// Weighted/Unweighted - values assigned to distances between vertices
+// Directed/Undirected - directions assigned to distances between vertices
+
+// A Tree can only have one path from one node to another
+// Undirected graphs are two-way connections
+// Directed graphs are often represented with arrows defining their direction to other nodes
+// A weighted graph has edges that contain values
+// We can use an adjacency matrix to represent a graph
+// We can also use an adjacency list to represent a graph 
+
+// * Adjacency List Pros
+// Can take up less space (in sparse graphs)
+// Faster to iterate over all the edges
+
+// * Adjacency List Cons
+// Can be slower to look up specific edge
+
+// * Adjacency Matrix Pros
+// Faster to look up specific edge
+
+// * Adjacency Matrix Cons
+// Takes up more space (in sparse graphs)
+// Slower to iterate over all edges
