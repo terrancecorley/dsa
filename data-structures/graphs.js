bcb6cb84fadf134c7afb39514d95a2b7e294827f
Terrance Corley <terrancecorley@outlook.com>
2023-06-10 16:04:15 -0700

    added basic methods to undirected graph

--- data-structures/graphs.js
+++ data-structures/graphs.js
@@ -32,3 +32,65 @@
 // * Adjacency Matrix Cons
 // Takes up more space (in sparse graphs)
 // Slower to iterate over all edges
+
+// Undirected Graph Class
+class Graph {
+    constructor() {
+        this.adjacencyList = {};
+    }
+
+    addVertex(vertex) {
+        // method does not handle dulicates, in real world we would probalby want to handle them
+        this.adjacencyList[vertex] = [];
+    }
+
+    removeVertex(vertex) {
+        // method does not contain error handling
+        while (this.adjacencyList[vertex].length) {
+            let adjacentVertex = this.adjacencyList[vertex].pop();
+            this.removeEdge(vertex, adjacentVertex);
+        }
+
+        delete this.adjacencyList[vertex];
+    } 
+
+    addEdge(vertex1, vertex2) {
+        // method does not contain error handling
+        // In a directed graph we would only push onto one array since the edge would have a direction 
+        this.adjacencyList[vertex1].push(vertex2);
+        this.adjacencyList[vertex2].push(vertex1);
+    }
+
+    removeEdge(vertex1, vertex2) {
+        // method does not contain error handling
+        // In a directed graph we would only remove from one array since the edge would have a direction
+        // Could use es6 .filter method
+        for (let i = 0; i < this.adjacencyList[vertex1].length; i++) {
+            let currentValue = this.adjacencyList[vertex1][i];
+            if (currentValue === vertex2) {
+                this.adjacencyList[vertex1].splice(i, 1);
+                break;
+            }
+        }
+
+        for (let i = 0; i < this.adjacencyList[vertex2].length; i++) {
+            let currentValue = this.adjacencyList[vertex2][i];
+            if (currentValue === vertex1) {
+                this.adjacencyList[vertex2].splice(i, 1);
+                break;
+            }
+        }
+    }
+}
+
+const g = new Graph();
+g.addVertex('tokyo');
+g.addVertex('san diego');
+g.addVertex('new york');
+g.addEdge('san diego', 'new york');
+console.log(g)
+g.removeVertex('san diego');
+console.log(g)
+
+const test = [1, 2];
+console.log(test[test.length - 1])
